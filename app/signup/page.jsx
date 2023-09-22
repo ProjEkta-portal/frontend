e56c9fcdd6d19ecfca52723428926dbf54cd4b4d
Devika Rajasekar <72180244+devika1402@users.noreply.github.com>
2023-09-22 11:26:34 +0530

    Update page.jsx

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,3 +1,19 @@
+"use client";
+import React, { useState } from "react";
+import PersonalInfo from "app/signup/personal-info/page.jsx";
+import AcademicDetails from "app/signup/academic-details/page.jsx";
+import InterestsSkills from "app/signup/interests-skills/page.jsx";
+
 export default function SignupPage() {
-  return <h1>Signup page</h1>;
+  const [currentStep, setCurrentStep] = useState(1);
+
+  const goToNextStep = () => setCurrentStep((prevStep) => prevStep + 1);
+
+  return (
+      <div className="min-h-screen flex justify-center items-center bg-white">
+        {currentStep === 1 && <PersonalInfo onNext={goToNextStep} />}
+        {currentStep === 2 && <AcademicDetails onNext={goToNextStep} />}
+        {currentStep === 3 && <InterestsSkills onNext={goToNextStep} />}
+      </div>
+  );
 }
