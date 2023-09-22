b796fcb2c42564594b45b7f1d80b78fc08973f28
Devika Rajasekar <72180244+devika1402@users.noreply.github.com>
2023-09-22 21:00:34 +0530

    Update InputField.jsx

--- components/login-and-signup/InputField.jsx
+++ components/login-and-signup/InputField.jsx
@@ -2,24 +2,31 @@
 import Image from "next/image";
 import React from "react";
 
-const InputField = ({ value, onChange, type, placeholder, iconSrc }) => (
-  <div className="mx-auto my-3 p-1 w-96 bg-[#F9F9FB] border-b border-black/25 relative">
-    <Image
-      src={iconSrc}
-      alt={`${placeholder} icon`}
-      width={18}
-      height={18}
-      className="absolute top-1/2 transform -translate-y-1/2 left-4"
-    />
+const InputField = ({ value, onChange, type, placeholder, iconSrc, width, height }) => (
+<div style={{ width: width, height: height }} className="bg-[#F9F9FB] border-b border-black/25 relative">
+      {iconSrc && (
+      <Image
+        src={iconSrc}
+        alt={`${placeholder} icon`}
+        width={24}
+        height={24}
+        className="absolute top-1/2 transform -translate-y-1/2 left-4"
+      />
+    )}
     <input
       value={value}
       onChange={onChange}
       type={type}
       placeholder={placeholder}
-      className="pl-20 pr-4 focus:outline-none bg-transparent text-[16px] leading-[16px]"
+      className="w-full h-full pl-20 pr-4 focus:outline-none bg-transparent text-[20px] leading-[24px] font-normal"
       required
     />
   </div>
 );
+
+InputField.defaultProps = {
+  width: '512px',
+  height: '42px'
+};
 
 export default InputField;
